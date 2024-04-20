be599e97fe894fcf39a514cce1b0af0f0b6cdc6b
Eros Secundus <erossecundus@gmail.com>
2024-04-20 20:20:03 -0300

    closes #3 - Tratando erro 404

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
+
 function search() {
   var username = document.getElementById("inputUserName").value;
   var url = `https://api.github.com/users/${username}`;
 
   $.getJSON(url, (user) => {
-    
-    document.getElementById("name").innerHTML     = user.name;
-    document.getElementById("html_url").innerHTML = user.html_url;
-    document.getElementById("location").innerHTML = user.location;
-    document.getElementById("avatar_url").innerHTML =
-    `<img src=${user.avatar_url} height="220" width="220" class="shadow rounded"></img>`;
-
-  })
+    showUser(user)
+    clearError();
+  }).fail( () => {
+    showUser({});
+    showError("Não encontrado!")
+  });
 }
+
+function showError(msg) {
+  document.getElementById("error").innerHTML =
+  `<div class="alert alert-danger mt-1" role="alert">${msg}/div>`;
+}
+
+function clearError() {
+  document.getElementById("error").innerHTML = "";
+}
+
+function showUser(user) {
+ 
+  document.getElementById("name").innerHTML     = user.name       || "";
+  document.getElementById("html_url").innerHTML = user.html_url   || "";
+  document.getElementById("location").innerHTML = user.location   || "";
+    
+  document.getElementById("avatar_url").innerHTML = user.avatar_url ? 
+      `<img src=${user.avatar_url} height="220" width="220" class="shadow rounded"></img>` : "";
+  
+}
+
+
+
