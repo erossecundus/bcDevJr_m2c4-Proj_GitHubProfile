81c28188ed1beda39b3ac244130cf99babc43016
Eros Secundus <erossecundus@gmail.com>
2024-04-20 21:48:50 -0300

    closes #4 - Histórico implementado

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 
+var usersHistory = [];
+
 function search() {
   var username = document.getElementById("inputUserName").value;
   var url = `https://api.github.com/users/${username}`;
 
   $.getJSON(url, (user) => {
-    showUser(user)
+    showUser(user);
     clearError();
+    
+    if(isNew(user)) {
+      save(user);
+      showHistory(user);
+    }
+      
   }).fail( () => {
     showUser({});
-    showError("Não encontrado!")
+    showError("Não encontrado!");
   });
 }
 
@@ -28,9 +36,23 @@ function showUser(user) {
   document.getElementById("location").innerHTML = user.location   || "";
     
   document.getElementById("avatar_url").innerHTML = user.avatar_url ? 
-      `<img src=${user.avatar_url} height="220" width="220" class="shadow rounded"></img>` : "";
+    `<img src=${user.avatar_url} height="220" width="220" class="shadow rounded"></img>` : "";
   
 }
 
+function save(user) {
+  usersHistory.push(user);
+}
 
+function isNew(user) {
+  return usersHistory.filter( (u) => u.login === user.login).length == 0;
+}
 
+function showHistory(user) {
+
+  document.getElementById("history").innerHTML += 
+    `<div class="col">
+      <img id="avatar_url" src=${user.avatar_url} height="110" width="110" class="shadow rounded">
+    </div>`
+
+}
